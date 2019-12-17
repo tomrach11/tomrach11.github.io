5958357618c0907b64176474b4ae711fd2f4ea4d
tomrach11 <tomrach11@gmail.com>
2019-12-16 21:36:19 -0500

    final lucky star

---
+++ codeAlongJavascript/luckySeven.js
@@ -0,0 +1,44 @@
+
+
+
+function play(){
+  var bet = Number(document.getElementById("money").value);
+  var rollCount = 0;
+  var rollCountAmountWon = 0;
+  var highest = 0;
+
+  var money = bet;
+  while (money > 0) {
+    var num = rollDice() + rollDice();
+    if (num == 7) {
+      money += 4;
+      rollCount += 1;
+        if (money >= highest) {
+          rollCountAmountWon = rollCount;
+          highest = money;
+        }
+    }
+    else {
+      money -= 1;
+      rollCount += 1;
+    }
+  }
+  document.getElementById("displayResult").style.display = "block";
+  document.getElementById("startingBet").innerText = bet;
+  document.getElementById("rollCount").innerText = rollCount;
+  document.getElementById("highest").innerText = highest;
+  document.getElementById("rollCountAmountWon").innerText = rollCountAmountWon;
+  console.log(rollCount);
+  console.log(rollCountAmountWon);
+  console.log(highest);
+
+}
+
+function validate(bet) {
+  if (bet < 1) {
+    alert("Please bet more than 1$");
+  }
+}
+function rollDice() {
+  return Math.ceil(Math.random()*6);
+}
